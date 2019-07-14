e9ad340e16e89dbcaecbf719d8a88c53ee830953
Marce Ramirez <marceram87@gmail.com>
2019-07-14 20:31:44 +0200

    Iteration 1, 2 and 3 done

--- app.js
+++ app.js
@@ -1,138 +1,141 @@
 'use strict';
 
 function main() {
+  // Iteration 1
 
-  //iteration 1
   // var main = document.querySelector('main');
-  // var getDog = function(){
-  //    return fetch('https://dog.ceo/api/breeds/image/random');
-  // }
-  // async function initDogApi(){
-  //   try{
-  //     var response = await getDog();
-  //     var data = await response.json();
-  //     var img = `
-  //       <img src="${data.message}" alt="Random Photo of a Dog"> 
-  //     `;
-  //     main.innerHTML = img;
-  //   }
-  //   catch(error){
-  //     console.log(error);
-  //   }
-  // }
-  // initDogApi();
-  
   // fetch('https://dog.ceo/api/breeds/image/random')
-  // .then( function(response){
-  //   response.json()
-  //   .then( function(data){
+  //   .then(function(res) {
+  //     return res.json();
+  //   })
+  //   .then(function(data){
   //     var img = `
-  //       <img src="${data.message}" alt="Random Photo of a Dog"> 
+  //       <img src="${data.message}" alt="Random Photo of a Dog">
   //     `;
   //     main.innerHTML = img;
   //   })
-  //   .catch( function(error){
+  //   .catch(function(error) {
   //     console.log(error);
-  //   })
-  // })
-  // .catch( function(error){
-  //   console.log(error);
-  // })
+  //   });
 
+  // function getDog() {
+  //   return fetch('https://dog.ceo/api/breeds/image/random');
+  // }
+  // async function initDogApi() {
+  //   try {
+  //     var res = await getDog();
+  //     var data = await res.json();
+  //     var img = `
+  //       <img src="${data.message}" alt="Random Photo of a Dog">
+  //     `;
+  //     main.innerHTML = img;
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // }
 
-  //iteration 2
+  // initDogApi();
+
+  // Iteration 2
+
+  // function getRates() {
+  //   return fetch('https://api.exchangeratesapi.io/latest');
+  // }
+
+  // function addEvent(currency, selectedValue) {
+  //   var dropDown = document.querySelector('#rates');
+  //   dropDown.addEventListener('change', function(event) {
+  //     selectedValue.innerHTML = currency[event.target.value];
+  //   });
+  // }
+
+  // async function initRates() {
+  //   try {
+  //     var response = await getRates();
+  //     var data = await response.json();
+  //     var markup =
+  //       '<section class="rates-section"><p>Rates: </p><select id="rates">';
+  //     for (var rate in data.rates) {
+  //       markup += `
+  //         <option>${rate}</option>
+  //       `;
+  //     }
+  //     markup += '</select></section>';
+  //     main.innerHTML = markup;
+  //     var selectedValue = document.createElement('p');
+  //     var rates = document.querySelector('.rates-section');
+  //     rates.appendChild(selectedValue);
+  //     addEvent(data.rates, selectedValue);
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // }
+
+  // initRates();
+
+  // Iteration 3
+
   var main = document.querySelector('main');
 
-  var getRates = function(base = 'EUR'){
-     return fetch(`https://api.exchangeratesapi.io/latest?base=${base}`);
+  function getPosts(numPosts) {
+    var proxyUrl = 'https://cors-anywhere.herokuapp.com/',
+      targetUrl = `https://dev-js-explained-api.pantheonsite.io/wp-json/wp/v2/posts?per_page=${numPosts}`;
+    return fetch(proxyUrl + targetUrl);
   }
-  
-  function addEvent(currency, selectedValue){
-    var dropDown = document.querySelector('#rates');
-    dropDown.addEventListener('change', function(event){
-      selectedValue.innerHTML = currency[event.target.value];
+
+  function getPost(id) {
+    var proxyUrl = 'https://cors-anywhere.herokuapp.com/',
+      targetUrl = `https://dev-js-explained-api.pantheonsite.io/wp-json/wp/v2/posts/${id}`;
+    return fetch(proxyUrl + targetUrl);
+  }
+
+  async function initPosts() {
+    try {
+      var posts = await getPosts(5);
+      var data = await posts.json();
+      var markup = '<section class="posts"><ul>';
+      data.forEach(function(post) {
+        markup += `<li>
+          <a data-id="${post.id}" href="${post.link}">${post.title.rendered}</a>
+        </li>`;
+      });
+      markup += '</ul></section>';
+      main.innerHTML = markup;
+      var postContent = document.createElement('section');
+      postContent.setAttribute('class', 'post-content');
+      main.appendChild(postContent);
+    } catch (error) {
+      console.log(error);
+    }
+    addEventsToPosts();
+  }
+
+  function addEventsToPosts() {
+    var postElements = document.querySelectorAll('[data-id]');
+    postElements.forEach(function(postElement) {
+      postElement.addEventListener('click', function(event) {
+        event.preventDefault();
+        return loadPost(event.target.dataset.id);
+      });
     });
   }
 
-// function changeBase(currency) {
-//   var newMarkup = '<p>Base: </p><select id="base">';
-//   for( var key in currency){
-//     newMarkup += `
-//           <option>${key}</option>
-//         `;
-//       }
-//       newMarkup += '</select>'
-//   //return newMarkup;
-//   console.log(newMarkup);
-//   //var dropDown = document.querySelector('')
-//}
-
-  // function buildDom(){
-     
-  // }
-
-  async function initRates(){
-    try{
-      var response = await getRates();
-      var data = await response.json();
-      var markup = '<p>Rates: </p><select id="rates">';
-      for( var rate in data.rates ){
-        markup += `
-          <option>${rate}</option>
-        `;
-      }
-      markup += '</select>'
-      
-      // var nodeToAppend = document.createElement('section');   
-      // nodeToAppend.innerHTML = markup;
-      main.innerHTML = markup;
-      var selectedValue = document.createElement('p'); 
-      main.appendChild(selectedValue);
-      addEvent(data.rates, selectedValue);
-      // changeBase(data.rates);
-      // main.appendChild();
-    }
-    catch(error){
+  async function loadPost(id) {
+    var postSection = document.querySelector('.post-content');
+    try {
+      var res = await getPost(id);
+      var postInfo = await res.json();
+      var markup = `
+        <h1>${postInfo.title.rendered}</h1>
+        ${postInfo.content.rendered}
+      `;
+      postSection.innerHTML = markup;
+    } catch (error) {
       console.log(error);
     }
   }
-  initRates();
 
-
-  //iteration3
-  // var getPosts = function(numPosts){
-  //   return fetch(`https://dev-js-explained-api.pantheonsite.io/wp-json/wp/v2/posts?per_page=5`);
-
-  //   //return fetch(`http://dev-js-explained-api.pantheonsite.io/wp-json/wp/v2/posts?per_page=${numPosts}`);
-  //   //  const request = new Request(`http://dev-js-explained-api.pantheonsite.io/wp-json/wp/v2/posts?per_page=${numPosts}`, {
-  //   //         method: 'GET',
-  //   //         mode: 'cors',
-  //   //         redirect: 'follow',
-  //   //         cache: 'no-cache',
-  //   //     })
-  //   //   return fetch(request);
-  // }
-  
-  // async function initPosts(){
-  //   try{
-  //     var response = await fetch(`https://dev-js-explained-api.pantheonsite.io/wp-json/wp/v2/posts?per_page=5`);
-  //     var posts = await response.json();
-  //     console.log(posts);
-  //     //<li><a data-id="POST_ID" href="POST_URL">POST_TITLE</a></li>.
-  //     var listItems = '<ul>';
-  //     posts.forEach( function(post){
-  //     listItems += `
-  //           <li><a data-id="${post[id]}" href="${post[link]}">${post[title][rendered]}</a></li>
-  //         `;
-  //     })
-  //     listItems += '</ul>'    }
-  //   catch(error){
-  //     console.log(error);
-  //   }
-  // }
-  // initPosts();
-
-
-};
+  initPosts();
+}
 
 window.addEventListener('load', main);
